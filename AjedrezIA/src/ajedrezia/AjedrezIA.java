502271d30c61cfbff15a08a06ae2d4c211ec936f
jjomena <joaquinmena.84@gmail.com>
2020-03-08 18:04:11 -0600

    Inicializar proyecto

---
+++ AjedrezIA/src/ajedrezia/AjedrezIA.java
@@ -0,0 +1,19 @@
+package ajedrezia;
+
+/**
+ *
+ * @author Joaquin
+ */
+public class AjedrezIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Tablero tb = new Tablero();
+        tb.generarTablero();
+        tb.setVisible(true);
+    }
+    
+}
