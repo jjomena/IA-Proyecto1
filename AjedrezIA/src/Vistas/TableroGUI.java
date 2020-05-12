45ad0d95618b894b8505e214c5bc044cd136a92c
jjomena <joaquinmena.84@gmail.com>
2020-05-11 19:46:17 -0600

    Agregar movimientos en log con casillas

--- AjedrezIA/src/Vistas/TableroGUI.java
+++ AjedrezIA/src/Vistas/TableroGUI.java
@@ -63,6 +63,16 @@ public class TableroGUI extends javax.swing.JFrame{
     private char colorJugador;
     private boolean turnoUsuario;
     
+    private String[][] margen = {{"H1","H2","H3","H4","H5","H6","H7","H8"},
+                                 {"G1","G2","G3","G4","G5","G6","G7","G8"},
+                                 {"F1","F2","F3","F4","F5","F6","F7","F8"},
+                                 {"E1","E2","E3","E4","E5","E6","E7","E8"},
+                                 {"D1","D2","D3","D4","D5","D6","D7","D8"},
+                                 {"C1","C2","C3","C4","C5","C6","C7","C8"},
+                                 {"B1","B2","B3","B4","B5","B6","B7","B8"},
+                                 {"A1","A2","A3","A4","A5","A6","A7","A8"}};
+    
+    
     /*Variables de Movimientos*/
     private ArrayList<Posicion> movimientosPosibles;
     
@@ -1056,7 +1066,7 @@ public class TableroGUI extends javax.swing.JFrame{
             }
             else{
                 descripcion = " Movimiento = "+nombrePieza+
-                    " ("+posInicialx+","+posInicialy+")"+"-"+"("+posFinalx+","+posFinaly+")";
+                    " ("+margen[posInicialx][posInicialy]+")"+"-"+"("+margen[posFinalx][posFinaly]+")";
             }
             PintarPieza(equipo,tipopieza,posFinalx,posFinaly);
             tablero.getCasillas()[posFinalx][posFinaly]
@@ -1101,7 +1111,7 @@ public class TableroGUI extends javax.swing.JFrame{
         }
         else{
             descripcion = " Movimiento = "+nombrePieza+
-                " ("+i+","+j+")"+"-"+"("+posFinalx+","+posFinaly+")";
+                " ("+margen[i][j]+")"+"-"+"("+margen[posFinalx][posFinaly]+")";
         }
         
         
